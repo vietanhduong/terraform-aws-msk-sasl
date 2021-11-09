b4112f2f9025f6319aed74ca2dcac6e089e15aad
Viet Anh Duong <vietanhs0817@gmail.com>
2021-11-09 14:00:39 +0700

    Update example

--- example/main.tf
+++ example/main.tf
@@ -3,33 +3,11 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-## Setup network
-### Create VPC
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-### Get AZs
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-### Create subnet per zone
-resource "aws_subnet" "subnet" {
-  count             = length(data.aws_availability_zones.available.names)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  cidr_block        = "10.0.${count.index}.0/24"
-  vpc_id            = aws_vpc.vpc.id
-
-}
-
-
 ## Init module
 module "msk-sasl" {
   source = "vietanhduong/msk-sasl/aws"
 
   cluster_name       = "msk"
-  vpc_id             = aws_vpc.vpc.id
   kafka_version      = "2.6.2"
   broker_type        = "kafka.t3.small"
   broker_volume_size = 512
@@ -44,5 +22,4 @@ module "msk-sasl" {
     "delete.topic.enable = true"
   ]
 
-  depends_on = [aws_subnet.subnet]
 }
