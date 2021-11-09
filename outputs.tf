36c61f36121a8eb9aebf926d016f124fa3faba10
Viet Anh Duong <vietanhs0817@gmail.com>
2021-11-09 14:11:37 +0700

    Update output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,25 @@
 output "proxy_private_key" {
-  value     = tls_private_key.proxy.private_key_pem
-  sensitive = true
+  description = "Proxy RSA Private key"
+  value       = tls_private_key.proxy.private_key_pem
+  sensitive   = true
 }
 
 output "proxy_public_ip" {
-  value = aws_instance.proxy.0.public_ip
+  description = "Proxy IP address"
+  value       = aws_instance.proxy.0.public_ip
+}
+
+output "zookeeper_connect_string" {
+  description = "MSK Zookeeper connection string"
+  value       = aws_msk_cluster.this.zookeeper_connect_string
+}
+
+output "bootstrap_brokers_sasl_scram" {
+  description = "MSK bootstrap brokers url"
+  value       = aws_msk_cluster.this.bootstrap_brokers_sasl_scram
+}
+
+output "cluster_arn" {
+  description = "MSK Cluster ARN"
+  value       = aws_msk_cluster.this.arn
 }
